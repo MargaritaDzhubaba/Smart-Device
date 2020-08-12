172dffc25fd8eff8290dfe33243abced17c82a1a
MargaritaDzhubaba <rikajubaba@gmail.com>
2020-08-12 12:19:16 +0300

    Добавляет разметку и стилизацию header, вариант, где фото плат находится в отдельном блоке и является img

--- gulpfile.js
+++ gulpfile.js
@@ -98,5 +98,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "images", "webp", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "images", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
