dc8b797bb9f2e1ef4f1ec5fb28370f39b98d1a60
MargaritaDzhubaba <rikajubaba@gmail.com>
2020-08-19 12:25:16 +0300

    Исправляет зависимости

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -91,6 +92,15 @@ gulp.task("copy", function () {
     ], {
       base: "source"
     })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function () {
+  return gulp.src([
+    "source/js/**"
+  ], {
+    base: "source"
+  })
   .pipe(gulp.dest("build"));
 });
 
